6bbad6d077d1dbad7aeb1b0c2c8f1dfc47c82ce9
Sajan Lawrence <sajanlawrence12345@gmail.com>
2026-01-03 21:46:49 +0530

    Initial Commit

---
+++ BloodConnect/BloodConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodConnectApp.swift
+//  BloodConnect
+//
+//  Created by Sajan Lawrence on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BloodConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
